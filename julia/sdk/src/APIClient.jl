7299c4da24afbca6c3fdc3bcaba97c8674774926
coinfer-serverside-ghaction <coinfer-serverside-ghaction@vectorly.com>
2025-08-13 11:26:25 +0000

    update from server/github-action(pr518)

--- julia/sdk/src/APIClient.jl
+++ julia/sdk/src/APIClient.jl
@@ -7,7 +7,7 @@ using Dates, TimeZones
 using OpenAPI
 using OpenAPI.Clients
 
-const API_VERSION = "0.0.1755081968+8fb58b4"
+const API_VERSION = "0.0.1755083317+8fb58b4"
 
 include("modelincludes.jl")
 
