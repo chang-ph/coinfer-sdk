049bf3f1c87635e3541dc3c91fe76fb2e171b7de
coinfer-serverside-ghaction <coinfer-serverside-ghaction@vectorly.com>
2025-11-06 00:22:24 +0000

    update from server/github-action(pr609)

--- julia/sdk/src/APIClient.jl
+++ julia/sdk/src/APIClient.jl
@@ -7,7 +7,7 @@ using Dates, TimeZones
 using OpenAPI
 using OpenAPI.Clients
 
-const API_VERSION = "0.0.1762337684+750370d"
+const API_VERSION = "0.0.1762386899+a5ef41a"
 
 include("modelincludes.jl")
 
--- python/sdk/openapi_client/configuration.py
+++ python/sdk/openapi_client/configuration.py
@@ -529,7 +529,7 @@ conf = openapi_client.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 0.0.1762337684+750370d\n"\
+               "Version of the API: 0.0.1762386899+a5ef41a\n"\
                "SDK Package Version: 1.0.0".\
                format(env=sys.platform, pyversion=sys.version)
 
